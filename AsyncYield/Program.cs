ece5bcd38ec79e6e633b3323bddf4c641413b34a
Santiago Botero Correa <sabotero@gmail.com>
2019-02-18 15:38:28 +0100

    Add project files.

---
+++ AsyncYield/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AsyncYield
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
