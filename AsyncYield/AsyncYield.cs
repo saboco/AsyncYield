335c4c4d636f69363a50fd897f1797fd282f1b58
Santiago Botero Correa <sabotero@gmail.com>
2019-02-22 17:15:51 +0100

    WIP: reading article

---
+++ AsyncYield/AsyncYield.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace AsyncYield
+{
+    public class YieldEnumerable<TItem> : IEnumerable<TItem>
+    {
+        private readonly Action<YieldEnumerator<TItem>> _iteratorMethod;
+
+        public YieldEnumerable(Action<YieldEnumerator<TItem>> iteratorMethod)
+        {
+            _iteratorMethod = iteratorMethod;
+        }
+
+        public IEnumerator<TItem> GetEnumerator()
+        {
+            var awaiter = new YieldEnumerator<TItem>();
+            _iteratorMethod(awaiter);
+            return awaiter;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    public class YieldEnumerator<TItem> : IEnumerator<TItem>, INotifyCompletion
+    {
+        private sealed class AbandonEnumeratorException : Exception { }
+        private Exception _exception;
+        private Action _continuation;
+
+        private TItem _nextValue;
+        private bool _hasNextValue;
+
+        private TItem _current;
+
+        public YieldEnumerator<TItem> GetAwaiter()
+        {
+            return this;
+        }
+
+        public void GetResult()
+        {
+            if (_exception != null)
+            {
+                var t = _exception;
+                _exception = null;
+                throw t;
+            }
+
+            _continuation = null;
+        }
+
+        public bool IsCompleted => false;
+
+        public YieldEnumerator<TItem> YieldReturn(TItem value)
+        {
+            if (_hasNextValue)
+                throw new InvalidOperationException();
+
+            _nextValue = value;
+            _hasNextValue = true;
+            return this;
+        }
+
+        public TItem Current => _current;
+
+        object IEnumerator.Current => _current;
+
+        public bool MoveNext()
+        {
+            if (!_hasNextValue)
+            {
+                _continuation?.Invoke();
+            }
+
+            if (!_hasNextValue)
+                return false;
+
+            _current = _nextValue;
+            _hasNextValue = false;
+            return true;
+        }
+
+        public void Reset()
+        {
+            _nextValue = default(TItem);
+            _continuation = null;
+        }
+
+        public void Dispose()
+        {
+            if (_continuation != null)
+            {
+                _exception = new AbandonEnumeratorException();
+
+                try
+                {
+                    _continuation();
+                }
+                catch (AbandonEnumeratorException x)
+                {
+                    Debug.Assert(_exception == null);
+                }
+
+                _continuation = null;
+            }
+        }
+
+        public void OnCompleted(Action continuation)
+        {
+            Debug.Assert(_continuation == null);
+            _continuation = continuation;
+        }
+    }
+}
--- AsyncYield/Program.cs
+++ AsyncYield/Program.cs
@@ -1,12 +1,62 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AsyncYield
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        public static async void MyIteratorMethod1(YieldEnumerator<int> e)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("A");
+            await e.YieldReturn(1);
+            Console.WriteLine("B");
+            await e.YieldReturn(2);
+            Console.WriteLine("C");
+            await e.YieldReturn(3);
+            Console.WriteLine("D");
+        }
+
+        public static async void MyIteratorMethod2(YieldEnumerator<int> e)
+        {
+            try
+            {
+                Console.WriteLine("A");
+                await e.YieldReturn(1);
+                Console.WriteLine("B");
+                await e.YieldReturn(2);
+                Console.WriteLine("C");
+                await e.YieldReturn(3);
+                Console.WriteLine("D");
+            }
+            finally
+            {
+                Console.WriteLine("Running finally");
+            }
+        }
+
+        public static async void MyIteratorMethod3(YieldEnumerator<int, string> e)
+        {
+            Console.WriteLine("A");
+            Console.WriteLine(await e.YieldReturn(1));
+            Console.WriteLine("B");
+            Console.WriteLine(await e.YieldReturn(2));
+            Console.WriteLine("C");
+            Console.WriteLine(await e.YieldReturn(3));
+            Console.WriteLine("D");
+        }
+
+        private static void Main(string[] args)
+        {
+            foreach (var i in new YieldEnumerable<int>(MyIteratorMethod1))
+                Console.WriteLine("Yielded: " + i);
+
+            foreach (var i in new YieldEnumerable<int>(MyIteratorMethod2))
+            {
+                Console.WriteLine("Yielded: " + i);
+            }
+
+
+            Console.ReadLine();
         }
     }
 }
